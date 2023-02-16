537478dd88d9123c265685e3ad35dc6167de57ed
ElyLYON1 <118751001+ElyLYON1@users.noreply.github.com>
2023-02-16 15:21:49 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+#include "Pixel.h"
+int main ()
+{
+    Pixel p1;
+    Pixel p2(25,55,127);
+
+    p1.AfficheStat();
+    p2.AfficheStat(); // test de doxygen , je cherche a verifier les constructeurs par copie et par défaut
+
+    p1.setgreen(20);
+    p1.AfficheStat();
+
+    return 0;
+}
